a22899d0fb299769acfc9747c71e3bc4792faed5
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2016-09-04 19:50:21 +0200

    Create userInfostartwrapper.bat

---
+++ userInfostartwrapper.bat
@@ -0,0 +1,3 @@
+copy /Y \\its.aau.dk\Fileshares\SupportTools\bin\UserInfo.hta %temp%\fisadko23223465sadaf3t35gdfasd325243.hta
+MSHTA.EXE %temp%\fisadko23223465sadaf3t35gdfasd325243.hta %1
+del %temp%\fisadko23223465sadaf3t35gdfasd325243.hta
